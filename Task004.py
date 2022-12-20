59f1b6aab1c7ba22d6421944bd292aad376a21ed
Anna <ankakras@yandex.ru>
2022-12-20 16:40:53 +0700

    Solution Task004

--- Task004.py
+++ Task004.py
@@ -4,16 +4,13 @@ import random
 some_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 random.shuffle(some_list)
 print(some_list)
-# Через random
 
+# Через random
 second_list = []
-for i in range(len(some_list)):
-    n = random.randrange(len(some_list))
-    i += n
-    if i < len(some_list):
-        second_list.append(some_list[i])
-        
-    else:
-        i -= len(some_list)
+while len(second_list) != len(some_list):
+    i = random.randrange(len(some_list))
+    if some_list[i] not in second_list:
         second_list.append(some_list[i])
 print(second_list)
+
+
